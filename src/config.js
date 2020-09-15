49196fe33e3772e573628e896c0f40173c983763
rohit mandyam <rohitrajasimhan@gmail.com>
2020-09-15 17:57:30 +0530

    first commit.

---
+++ src/config.js
@@ -0,0 +1,8 @@
+let MYTUBE_CONFIG = {
+    API_KEY: "AIzaSyDRhBC778y9Wa2NeVj_pTqCrkD0T811AvY",
+    BASE_URL: 'https://www.googleapis.com/youtube/v3',
+    CLIENT_ID: '864940698266-hlihqjhgbst2ltoeb0elgld3bgpdp0eb.apps.googleusercontent.com'
+};
+
+export default MYTUBE_CONFIG;
+
