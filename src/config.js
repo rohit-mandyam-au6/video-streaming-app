ddd3c596ce46ffa68d7c5c31e1c59e76273f915a
Rohit Mandyam <42966443+rohit-mandyam-au6@users.noreply.github.com>
2020-09-15 18:38:09 +0530

    Update config.js
    4th commit

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 let MYTUBE_CONFIG = {
-    API_KEY: "AIzaSyDRhBC778y9Wa2NeVj_pTqCrkD0T811AvY",
+    API_KEY: "AIzaSyDRbO8Haj-pfEU6liqs4VL5tEyPIuG_UlI",
     BASE_URL: 'https://www.googleapis.com/youtube/v3',
-    CLIENT_ID: '864940698266-hlihqjhgbst2ltoeb0elgld3bgpdp0eb.apps.googleusercontent.com'
+    CLIENT_ID: '59974692553-pm127a9u90sd23fa10uhccrjenlr80dh.apps.googleusercontent.com'
 };
 
 export default MYTUBE_CONFIG;
