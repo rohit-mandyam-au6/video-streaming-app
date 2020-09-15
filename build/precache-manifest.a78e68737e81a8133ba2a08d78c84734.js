0acd96f26cfd5df14b8a6abae904e97920249da6
Rohit Mandyam <42966443+rohit-mandyam-au6@users.noreply.github.com>
2020-09-15 18:17:56 +0530

    Add files via upload
    second commit

---
+++ build/precache-manifest.a78e68737e81a8133ba2a08d78c84734.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d445d07f6e6a15bb7938a24e618d69a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0f90bf4aa57360853be",
+    "url": "/static/css/main.abd58810.chunk.css"
+  },
+  {
+    "revision": "cd4641ab908f1746ef41",
+    "url": "/static/js/2.3e591748.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3e591748.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0f90bf4aa57360853be",
+    "url": "/static/js/main.e2c23b08.chunk.js"
+  },
+  {
+    "revision": "6a040985a61f21896475",
+    "url": "/static/js/runtime-main.8ccca7a7.js"
+  },
+  {
+    "revision": "37194ebb0d43ab3274bf6760a7e95262",
+    "url": "/static/media/Loading-Image-1.37194ebb.gif"
+  }
+]);
